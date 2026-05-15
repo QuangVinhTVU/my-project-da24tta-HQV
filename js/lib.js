c9b3e24f74469b3afca034345fc8fb7a6eb829b9
vinh <huynhquangvjnh84@gmail.com>
2026-05-15 10:17:45 +0700

    Update my project

---
+++ js/lib.js
@@ -0,0 +1,22 @@
+/* Học viết Css */ 
+*{
+	margin : 0;
+	padding : 0; 
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 14pt; /* Định dạng cỡ chữ */ 
+	line-height: 1.5; /* Định dạng cách dòng*/
+	
+}
+div.container{/* định dạng thẻ container*/
+	margin : 10px 100px 0px;
+	background-color: #00FFFF ;
+	padding : 20px;
+	
+}
+ h3.tieude{
+		font-size: 18px;
+		color: while;
+		text-transform: uppercase;
+		letter-spacing: 3px;
+ }
+ 
