b7f72eea607179ae45633c94765c0f728c965d3f
vinh <huynhquangvjnh84@gmail.com>
2026-06-12 10:51:10 +0700

    update 120626

---
+++ js/data.js
@@ -0,0 +1,40 @@
+const product = {
+                    name: "Iphone 14 Pro Max",
+                    price: "27.000.000đ",
+                     description: "Iphone 14 Pro Max là một chiếc điện thoại thông minh cao cấp được sản xuất bởi Apple vào năm 2022,... ",
+                    img: "../assets/images/ip14prm.jpg",
+                    linkProduct: "chi-tiet.html"
+                };
+
+// lưu thông tin nhiều đối tượng thì dùng mảng để lưu trữ
+const products = [
+    {
+        name: "Iphone 14 Pro Max", 
+        price: "27.000.000đ",
+        description: "Iphone 14 Pro Max là một chiếc điện thoại thông minh cao cấp được sản xuất bởi Apple vào năm 2022,... ",
+        img: "../assets/images/ip14prm.jpg",
+        linkProduct: "chi-tiet1.html"
+    }
+    {
+        name: "Samsung Galaxy S23 Ultra",
+        price: "25.000.000đ",
+        description: "Samsung Galaxy S23 Ultra là một chiếc điện thoại thông minh cao cấp được sản xuất bởi Samsung vào năm 2023,... ",
+        img: "../assets/images/ss23ultra.jpg",
+        linkProduct: "chi-tiet2.html"
+    }
+    {
+        name: "Xiaomi 17 Pro Max",
+        price: "20.000.000đ",   
+        description: "Xiaomi 17 Pro Max là một chiếc điện thoại thông minh cao cấp được sản xuất bởi Xiaomi, sản phẩm này được thương hiệu trình làng tại thị trường Trung Quốc vào ngày 25 tháng 9 năm 2025.,... ",
+        img: "../assets/images/xiaomi-17-pro-max.jpg",
+        linkProduct: "chi-tiet3.html"
+    }
+       
+    {
+        name: "oppo Find X5 Pro",
+        price: "22.000.000đ",
+        description: "oppo Find X5 Pro là một chiếc điện thoại thông minh cao cấp được sản xuất bởi oppo vào năm 2022,... ",
+        img: "../assets/images/oppo-find-x5-pro.jpg",
+        linkProduct: "chi-tiet4.html"
+    }
+];
--- js/lib.js
+++ js/lib.js
@@ -1,3 +1,24 @@
+/* mã nguồn javascript*/
+const product = {
+                    name: "Iphone 14 Pro Max",
+                    price: "27.000.000đ",
+                     description: "Iphone 14 Pro Max là một chiếc điện thoại thông minh cao cấp được sản xuất bởi Apple vào năm 2022,... ",
+                    img: "../assets/images/ip14prm.jpg",
+                    linkProduct: "chi-tiet.html"
+                };
+
+function  inBBC(n)
+{
+    result = "";
+    let i = 1;
+    while(i <= 10)
+    {
+        result += `S(${n}) x S(${1}) = S(${n * 1}) <br>`;
+        i++;
+    }
+    document.getElementById("result").innerHTML = result;
+}
+
 function createItem(obj) // obj là đối tượng thực tế đang quản lý thông tin sản phẩm cần thêm
 { 
     const item = document.createElement("div"); //<div></div>
@@ -19,20 +40,51 @@ function createItem(obj) // obj là đối tượng thực tế đang quản lý
 
     //tạo 3 đối tượng <p> : name, price, description và 1 đối tượng <a>
         const name = document.createElement("p"); //<p></p>
-        name.innerHTML = obj.name; 
+        name.innerHTML = obj.name; // <p>iphone 14 Pro Max</p>
 
         const price = document.createElement("p"); //<p></p>
-        price.innerHTML = obj.price; // <p>giá sản phẩm</p>
+        price.innerHTML = obj.price; // <p> 30.000.000đ</p>
 
         const description = document.createElement("p"); //<p></p>
-        description.innerHTML = obj.description; // <p>mô tả sản phẩm</p> 
+        description.innerHTML = obj.description; // <p> ip14prm do apple sản xuất</p> 
 
         const link = document.createElement("a"); //<a></a>
-        link.setAttribute("href", obj.link); //<a href="link sản phẩm">Xem chi tiết</a>
-        link.innerHTML = "Xem chi tiết";
+        link.innerHTML = "Xem chi tiết"; // <a>Xem chi tiết</a>
+        link.setAttribute("href", obj.linkProduct); //<a href="link sản phẩm">Xem chi tiết</a>
+    //đưa 4 đối tượng vừa tạo vào khung container-info
+    containerInfo.appendChild(name);
+    containerInfo.appendChild(price);
+    containerInfo.appendChild(description);
+    containerInfo.appendChild(link);
 
     //chèn hai khung chứa vào container-item
     item.appendChild(containerImage);
     item.appendChild(containerInfo);
 
+    // đưa container-item vào product-list
+    document.getElementById("product-list").appendChild(item);
+
 }
+ 
+function createItemV2(obj) 
+{
+    const list = document.getElementById("product-list"); // truy suất khuung chưa DS sản phẩm
+    list.innerHTML = `
+        <div class="col">
+            <div class="card product-item"> 
+            <div class="product-image">
+                <img class="card-img-top" src="${obj.img}" alt="${obj.name}">
+            </div>
+            <div class="card-body product-info text-center">
+            <h4 class="card-title text-danger">${obj.name}</h4>
+            <h5 class="card-text">${obj.price}</h5>
+            <p style="text-align: justify; line-height: 1.5;" class="card-text"> ${obj.description}</p>
+            <a href="${obj.linkProduct}" class="btn btn-info">Xem chi tiết</a>
+            </div>
+        </div>
+    </div>
+            
+     `;
+  
+}
+
